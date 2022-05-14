880afcde074f1f0844fd341769f4b78a46b449ed
Yasser Abd Elghany <yasser.m22291@gmail.com>
2022-05-14 12:46:53 +0200

    Time: 19 ms (5.85%), Space: 12.6 MB (69.48%) - LeetHub

---
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -0,0 +1,11 @@
+
+ 
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        if(root->val == root->left->val + root->right->val)
+            return true;
+    
+        return false;
+    }
+};
